34e12d72969f4c12bce42a7ec790eddbbfb5805a
nachomi24 <66391621+nachomi24@users.noreply.github.com>
2022-05-13 09:53:17 -0500

    Fixed crash game now working

--- pacman.py
+++ pacman.py
@@ -16,17 +16,18 @@ Nahomi Plata
 from random import choice
 from turtle import *
 from freegames import floor, vector
+import random
 
 state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
-pacman = vector(-50, -80)
+pacman = vector(-40, 0)
 ghosts = [
-    [vector(-180, 160), vector(5, 0),'cyan'],
-    [vector(-180, -160), vector(0, 5),'magenta'],
-    [vector(100, 160), vector(0, -5),'red'],
-    [vector(100, -160), vector(-5, 0),'coral'],
+        [vector(-180, 160), vector(5, 0)],
+    [vector(-180, -160), vector(0, 5)],
+    [vector(100, 160), vector(0, -5)],
+    [vector(100, -160), vector(-5, 0)],
 ]
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
